700bb01590e785815fcd10e35dc8679b97930f12
joejacob <joejacob95@gmail.com>
2016-11-18 17:01:36 +0100

    icon now changes picture on click

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,3 +1,15 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+var currIcon = 0;
 
+// updates icon image
+function updateIcon() {
+	chrome.browserAction.setIcon({path: "./../icons/ticker_sticker_" + currIcon + ".png"});
+	currIcon = (currIcon + 1) % 2;
+
+}
+
+// turning extension on and off
+chrome.browserAction.onClicked.addListener(function(tab) {
+	updateIcon();
 });
+
+updateIcon();
