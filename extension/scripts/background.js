7702079afeb21dc8d628de5256c92ce501c23e01
Joe Jacob <jcj26@duke.edu>
2017-09-17 23:47:02 -0400

    add analytics

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,3 +1,10 @@
+// track page views
+var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-105747972-2']); _gaq.push(['_trackPageview']);
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 var currIcon = 0;
 updateState(null);
 
@@ -15,6 +22,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 function updateState(tab) {
 	updateIcon();
 	chrome.storage.sync.set({'state': currIcon}, function() {
-		//message('Toggled on/off');
+		//message('Toggled on/off');e
 	});
 }
